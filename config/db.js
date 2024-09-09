be70f435e3950cfa986ce040a9f0e388cf132419
Ram Rathore <kushirathore2014@gmail.com>
2024-09-09 23:42:34 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "1234Ram", {
   host: process.env.DB_HOST,
   dialect: 'postgres',
-  logging: false,
 });
 
 module.exports = sequelize;
