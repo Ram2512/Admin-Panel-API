bfd79b095da008461b89aa9ab0c685288a1067b7
Ram Rathore <kushirathore2014@gmail.com>
2024-09-10 00:51:14 +0530

    neon connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,16 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "1234Ram", {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres',
+// });
+
+// module.exports = sequelize;
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "1234Ram", {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DB_LINK,{
   dialect: 'postgres',
 });
 
